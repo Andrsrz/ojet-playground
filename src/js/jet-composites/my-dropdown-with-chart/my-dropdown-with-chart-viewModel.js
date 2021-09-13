485af9dc305e002ede69fc81e82cb5ebecdc54d4
Andres Ruiz <andres.ruiz@oracle.com>
2021-09-13 11:27:06 -0500

    Trying events and methods, doesn't work

--- src/js/jet-composites/my-dropdown-with-chart/my-dropdown-with-chart-viewModel.js
+++ src/js/jet-composites/my-dropdown-with-chart/my-dropdown-with-chart-viewModel.js
@@ -14,43 +14,47 @@ define(
     function (ko, componentStrings, Context, ArrayDataProvider) {
     
     function ExampleComponentModel(context) {
-        //At the start of your viewModel constructor
-        var busyContext = Context.getContext(context.element).getBusyContext();
-        var options = {"description": "Web Component Startup - Waiting for data"};
-        this.busyResolve = busyContext.addBusyState(options);
+      function sayHi(event){
+        alert('Hi');
+        element.dispatchEvent(new CustomEvent('sayHi'));
+      }
+      //At the start of your viewModel constructor
+      var busyContext = Context.getContext(context.element).getBusyContext();
+      var options = {"description": "Web Component Startup - Waiting for data"};
+      this.busyResolve = busyContext.addBusyState(options);
 
-        this.composite = context.element;
+      this.composite = context.element;
 
-        // Types objects to use on selector
-        let types = [
-          { value: 'pie', label: 'Pie'},
-          { value: 'bar', label: 'Bar'}
-        ];
+      // Types objects to use on selector
+      let types = [
+        { value: 'pie', label: 'Pie'},
+        { value: 'bar', label: 'Bar'}
+      ];
 
-        //  Set types objects into array to use it
-        this.chartTypes = new ArrayDataProvider(types, { keyAttributes: 'value'});
-        this.val = context.properties.chartType ? ko.observable(context.properties.chartType) : ko.observable("pie");
+      //  Set types objects into array to use it
+      this.chartTypes = new ArrayDataProvider(types, { keyAttributes: 'value'});
+      this.val = context.properties.chartType ? ko.observable(context.properties.chartType) : ko.observable("pie");
 
-        // Dummy data to show on chart
-        let chartData = [
-          { "id": 0, "series": "Baseball", "group": "Group A", "value": 42 },
-          { "id": 1, "series": "Baseball", "group": "Group B", "value": 34 },
-          { "id": 2, "series": "Bicycling", "group": "Group A", "value": 55 },
-          { "id": 3, "series": "Bicycling", "group": "Group B", "value": 30 },
-          { "id": 4, "series": "Skiing", "group": "Group A", "value": 36 },
-          { "id": 5, "series": "Skiing", "group": "Group B", "value": 50 },
-          { "id": 6, "series": "Soccer", "group": "Group A", "value": 22 },
-          { "id": 7, "series": "Soccer", "group": "Group B", "value": 46 }
-        ];
+      // Dummy data to show on chart
+      let chartData = [
+        { "id": 0, "series": "Baseball", "group": "Group A", "value": 42 },
+        { "id": 1, "series": "Baseball", "group": "Group B", "value": 34 },
+        { "id": 2, "series": "Bicycling", "group": "Group A", "value": 55 },
+        { "id": 3, "series": "Bicycling", "group": "Group B", "value": 30 },
+        { "id": 4, "series": "Skiing", "group": "Group A", "value": 36 },
+        { "id": 5, "series": "Skiing", "group": "Group B", "value": 50 },
+        { "id": 6, "series": "Soccer", "group": "Group A", "value": 22 },
+        { "id": 7, "series": "Soccer", "group": "Group B", "value": 46 }
+      ];
 
-        // Set chart data objects into array to use it
-        this.chartDataProvider = new ArrayDataProvider(chartData, { keyAttributes: 'id'});
+      // Set chart data objects into array to use it
+      this.chartDataProvider = new ArrayDataProvider(chartData, { keyAttributes: 'id'});
 
-        this.properties = context.properties;
-        this.res = componentStrings['my-dropdown-with-chart'];
+      this.properties = context.properties;
+      this.res = componentStrings['my-dropdown-with-chart'];
 
         //Once all startup and async activities have finished, relocate if there are any async activities
-        this.busyResolve();
+      this.busyResolve();
     };
     
     //Lifecycle methods - uncomment and implement if necessary 
